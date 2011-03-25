d2039853c77061883e8bc6c9169f12fc8461b1c7
Joseph Chysler <jdchrysl@svsu.edu>
2011-03-25 16:00:48 -0400

    Try something else

--- janitor.js
+++ janitor.js
@@ -53,7 +53,7 @@ zmJanitor.prototype._rpcCallback = function(p1, response) {
                           ZmToast.FADE_OUT];
 
     appCtxt.getAppController().setStatusMsg("Hello");
-  if (oldEmails == 0 && oldUnreads == 0) {
+  if (oldEmails != 0 && oldUnreads != 0) {
     appCtxt.getAppController().setStatusMsg(warningMessage, ZmStatusView.LEVEL_CRITICAL, null, warningAnimation);
   }
 }
