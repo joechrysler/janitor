f0d95c5667188608abb1d48f7ef974a05f20d0c3
Joseph Chysler <jdchrysl@svsu.edu>
2011-04-11 14:24:13 -0400

    try something

--- janitor.js
+++ janitor.js
@@ -26,12 +26,12 @@
     var username        = appCtxt.get(ZmSetting.USERNAME).split("@")[0];
     var callbck         = new AjxCallback(this, this._rpcCallback);
     /*var _types          = new AjxVector();*/
-    var yearAgo         = this._buildHistoricalDate(1); //tk change to 365
+    var yearAgo         = this._buildHistoricalDate(1); //tk change to 365 //tk change to 365
     var ninetyDaysAgo   = this._buildHistoricalDate(90);
     /*_types.add("MAIL");*/
 
     appCtxt.getSearchController().search({
-      query:      'before:' + yearAgo,
+      query:      'before:' + yearAgo + ' not from:' + username,
       userText:   true,
       limit:      9999,
       /*types:      _types,*/
